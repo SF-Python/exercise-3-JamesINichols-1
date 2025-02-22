f1a75aae672adeddd8cbb37e8f589959578987a6
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:06:27 -0500

    Cinquez

--- exercise3.py
+++ exercise3.py
@@ -1,22 +1,32 @@
-# exercise3.py
-def main():
-    hex_digits = input("Please enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    if hex_digits == "00:00:17":
-        manufacturer = "Oracle"
-    elif hex_digits == "00:07:E9":
-        manufacturer = "Intel Corporation"
-    elif hex_digits == "04:27:28":
-        manufacturer = "Microsoft Corporation"
-    elif hex_digits == "04:26:65":
-        manufacturer = "Apple, Inc."
-    elif hex_digits == "04:33:89":
-        manufacturer = "Huawei Technologies Co.,Ltd"
-    elif hex_digits == "00:00:0C":
-        manufacturer = "Cisco Systems, Inc"
-    else:
-        manufacturer = "<Not found>"
+# cinquez johnson (cinjohnson81)
+# W25 COP2002.0T1
+# Fabruary 8, 2025
+# If Statements
+# Program to determine the manufacturer of a NIC card
 
-    print("For the MAC address, the manufacturer is " + manufacturer)
 
-if __name__ == "__main__":
-    main()
+print("MAC Manufacturer Program")
+
+print("________________________")
+
+print()
+
+MAC_addresses = input ("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+
+if MAC_addresses == "00:00:17":
+    print ("For 00:00:17 the MAC manufacturer is Oracle")
+
+elif MAC_addresses == "00:07:E9":
+    print ("For 00:07:E9 the MAC manufacturer is Microsoft Corporation")
+
+elif MAC_addresses == "04:26:65":
+    print ("For 04:26:65 the MAC manufacturer is Apple, Inc.")
+
+elif MAC_addresses == "04:33:89":
+    print ("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd")
+
+elif MAC_addresses == "00:00:0C":
+    print ("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc")
+
+else:
+    print ("Unknown")
