0239d5f5d0fffa02f44517473f7e8869b29a34a7
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:30:12 -0500

    Hannah

--- exercise3.py
+++ exercise3.py
@@ -1,33 +1,34 @@
-# Ishaan Patel
-# COP2002-0T1
-# Feb 16, 2025
-# Exercise 3
-# Program asks the user for a mac address as input then uses if elif statements to determine the
-# manufacturer of the MAC address.
+# Hannah Raynard (GHUN DarkenedI)
+# COP 2002 OT1
+# 2025-02-14
+# MAC manufacturer program
+# This program will prompt the user for input then match it to specific variables
 
+# program start
+print("MAC Manufacterer Program")
+print("------------------------")
+print()
+# Prompts user for input
+userPrompt=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
-def main():
-    # Header
-    print("MAC Manufacturer Program")
-    print("------------------------")
-    print("")
-    
-    #input from user
-    userMac = input("Enter the first 6 values of the Mac address (format as XX:XX:XX):  ")
+# Prints prompt to the screen
+print(userPrompt)
 
-    #if elif and else statements
-    if userMac == "00:00:17":
-        print(f"For {userMac} the MAC manufacturer is Oracle.")
-    elif userMac == "00:07:E9":
-        print(f"For {userMac} the MAC manufacturer is Intel Corportation.")
-    elif userMac == "04:27:28":
-        print(f"For {userMac} the MAC manufacturer is Microsoft Corportation.")
-    elif userMac == "04:26:65":
-        print(f"For {userMac} the MAC manufacturer is Apple Inc.")
-    elif userMac == "04:33:89":
-        print(f"For {userMac} the MAC manufacturer is Huawei Technologies Co. Ltd.")
-    else:
-        print("This MAC manufacturer is unknown.")
+# List for keeping track of all the manufacturers
+manufacturerList=["Oracle","Intel Corporation", "Microsoft Corporation","Apple, Inc.", "Huawei Technologies Co., Ltd", "Cisco Systems, Inc"]
 
-if (__name__ == "__main__"):
-    main()
+# if/elif/else statements
+if userPrompt=="00:00:17" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[0]}")
+elif userPrompt=="00:07:E9" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[1]}")
+elif userPrompt=="04:27:28" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[2]}")
+elif userPrompt=="04:26:65" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[3]}")
+elif userPrompt=="04:33:89" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[4]}")
+elif userPrompt=="00:00:0C" :
+    print(f"for {userPrompt} the MAC manufacterer is {manufacturerList[5]}")
+else:
+    print("Not found.")
