4b6c71a60495862f550a73acec4915a25dae2e73
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:54:24 -0500

    Samantha

--- exercise3.py
+++ exercise3.py
@@ -1,19 +1,17 @@
-def main():
-    answer=input("Enter the first 6 hex values of the MAC adress (format as XX:XX:XX): ")
-    if (answer=="00:00:17"):
-        manufacturer="Oracle"
-    elif (answer=="00:07:E9"):
-        manufacturer="Intel Corporation"
-    elif (answer=="04:27:28"):
-        manufacturer="Microsoft Corporation"
-    elif (answer=="04:26:65"):
-        manufacturer="Apple, Inc."
-    elif (answer=="04:33:89"):
-        manufacturer="Huawei Technologies Co.,Ltd"
-    elif (answer=="00:00:0C"):
-        manufacturer="Cisco Systems, Inc"
-    else: 
-        manufacturer="Unknown"
-    print(f"For {answer} the MAC manufacturer is {manufacturer}.")
-if (__name__=="__main__"):
-    main()
+print("MAC Manufacturer Program")
+print("------------------------")
+print()
+Oracle="00:00:17"
+Intel_Corporation="00:07:E9"
+Microsoft_Corpoation="04:27:28"
+Apple="04.26.65"
+HuaweiTechnoloies_Co_Ltd="04:33:89"
+Cisco_SystemsInc="00.00.0C"
+Not_valid_value_or_not_found="Unknown"
+
+
+x=input("Enter the first 6 hex values of the MAC address(format as {XX.XX.XX}): ")
+if Intel_Corporation==x.upper():
+
+    print("For 00:07:E9 the MAC manufacturer is Intel Corporation." )
+
