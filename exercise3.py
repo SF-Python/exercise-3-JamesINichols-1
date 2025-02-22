ba046d4e05cfe56b887b844c68fd91be7b54eddf
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:17:27 -0500

    Ian

--- exercise3.py
+++ exercise3.py
@@ -1,32 +1,34 @@
-# cinquez johnson (cinjohnson81)
-# W25 COP2002.0T1
-# Fabruary 8, 2025
-# If Statements
-# Program to determine the manufacturer of a NIC card
+# Ian James(JamesIan20)
+# COP2002.0T1 Spring 2025
+# 1/26/2025
+# Exercise 2: Variables
+# The purpose of this program is to ensure students know how if functions work with loops user inputs
 
+def main(): # Used to simplify code down only to call one function into print
+   
+    # Header
+    print("MAC Manufacturer Program")
+    print("------------------------")
+    print() # Prints a blank line before the input
+    
+    # List of known hex digits and their corresponding manufacturers
+    hex_digits = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
+    manufacturers = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
 
-print("MAC Manufacturer Program")
+    # Prompt the user to enter the hex digits
+    hex_input = input("Please enter the first 6 hex digits (format as XX:XX:XX): ")
 
-print("________________________")
+    # Initialize the manufacturer as Unknown
+    manufacturer = "Unknown"
 
-print()
+    # Loop to find the manufacturer
+    for i in range(len(hex_digits)):
+        if hex_input == hex_digits[i]:
+            manufacturer = manufacturers[i]
 
-MAC_addresses = input ("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    # Print the result
+    print(f"For {hex_input} the MAC manufacturer is {manufacturer}")
 
-if MAC_addresses == "00:00:17":
-    print ("For 00:00:17 the MAC manufacturer is Oracle")
-
-elif MAC_addresses == "00:07:E9":
-    print ("For 00:07:E9 the MAC manufacturer is Microsoft Corporation")
-
-elif MAC_addresses == "04:26:65":
-    print ("For 04:26:65 the MAC manufacturer is Apple, Inc.")
-
-elif MAC_addresses == "04:33:89":
-    print ("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd")
-
-elif MAC_addresses == "00:00:0C":
-    print ("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc")
-
-else:
-    print ("Unknown")
+# To call the function into effect
+if __name__ == "__main__": 
+    main()
