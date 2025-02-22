691c08e81713a8ad3d71188b36d53a77d44ee571
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:21:10 -0500

    Daniel

--- exercise3.py
+++ exercise3.py
@@ -1,34 +1,26 @@
-# Ian James(JamesIan20)
-# COP2002.0T1 Spring 2025
-# 1/26/2025
-# Exercise 2: Variables
-# The purpose of this program is to ensure students know how if functions work with loops user inputs
-
-def main(): # Used to simplify code down only to call one function into print
-   
-    # Header
+def main():
+    # Display program header
     print("MAC Manufacturer Program")
     print("------------------------")
-    print() # Prints a blank line before the input
     
-    # List of known hex digits and their corresponding manufacturers
-    hex_digits = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
-    manufacturers = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
+    # Ask the user for input
+    mac_address = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
-    # Prompt the user to enter the hex digits
-    hex_input = input("Please enter the first 6 hex digits (format as XX:XX:XX): ")
+    # List of known MAC address prefixes and corresponding manufacturers
+    mac_codes = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
+    manufacturers = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.",
+                     "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
 
-    # Initialize the manufacturer as Unknown
-    manufacturer = "Unknown"
+    # Remove any unwanted spaces or characters in the input
+    mac_address = mac_address.strip()
 
-    # Loop to find the manufacturer
-    for i in range(len(hex_digits)):
-        if hex_input == hex_digits[i]:
-            manufacturer = manufacturers[i]
+    # Check if the input MAC address matches any known MAC address prefix
+    if mac_address in mac_codes:
+        index = mac_codes.index(mac_address)
+        print(f"For {mac_address} the MAC manufacturer is {manufacturers[index]}.")
+    else:
+        print(f"For {mac_address} the MAC manufacturer is Unknown.")
 
-    # Print the result
-    print(f"For {hex_input} the MAC manufacturer is {manufacturer}")
-
-# To call the function into effect
-if __name__ == "__main__": 
+# Call the main function
+if __name__ == "__main__":
     main()
