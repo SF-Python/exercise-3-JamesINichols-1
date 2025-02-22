42b8f873d93cb0f9d2c1d9281a2034e5704e96ff
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 22:40:42 -0500

    Rinat

--- exercise3.py
+++ exercise3.py
@@ -1,27 +1,22 @@
+# exercise3.py
 def main():
-    print("           MAC Manufacturer Program")
-    print("------------------------")
-    print("")
-    
-    # List of MAC prefixes and manufacturers
-    mac_prefixes = [
-        "00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"
-    ]
-    manufacturers = [
-        "Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"
-    ]
-    
-    # Get user input
-    mac_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    
-    # Check if input is in list
-    if mac_input in mac_prefixes:
-        index = mac_prefixes.index(mac_input)
-        manufacturer = manufacturers[index]
+    hex_digits = input("Please enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    if hex_digits == "00:00:17":
+        manufacturer = "Oracle"
+    elif hex_digits == "00:07:E9":
+        manufacturer = "Intel Corporation"
+    elif hex_digits == "04:27:28":
+        manufacturer = "Microsoft Corporation"
+    elif hex_digits == "04:26:65":
+        manufacturer = "Apple, Inc."
+    elif hex_digits == "04:33:89":
+        manufacturer = "Huawei Technologies Co.,Ltd"
+    elif hex_digits == "00:00:0C":
+        manufacturer = "Cisco Systems, Inc"
     else:
-        manufacturer = "Unknown"
-    
-    print(f"For {mac_input} the MAC manufacturer is {manufacturer}.")
-    
+        manufacturer = "<Not found>"
+
+    print("For the MAC address, the manufacturer is " + manufacturer)
+
 if __name__ == "__main__":
     main()
