4cb35e008d52fb5b0d458dbd41a8bc81c8777f60
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-21 23:26:11 -0500

    Ishaan

--- exercise3.py
+++ exercise3.py
@@ -1,28 +1,33 @@
+# Ishaan Patel
+# COP2002-0T1
+# Feb 16, 2025
+# Exercise 3
+# Program asks the user for a mac address as input then uses if elif statements to determine the
+# manufacturer of the MAC address.
+
+
 def main():
-    # Print program header
+    # Header
     print("MAC Manufacturer Program")
     print("------------------------")
-    print()
+    print("")
+    
+    #input from user
+    userMac = input("Enter the first 6 values of the Mac address (format as XX:XX:XX):  ")
 
-    # Get user input for MAC hex values
-    mac_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    #if elif and else statements
+    if userMac == "00:00:17":
+        print(f"For {userMac} the MAC manufacturer is Oracle.")
+    elif userMac == "00:07:E9":
+        print(f"For {userMac} the MAC manufacturer is Intel Corportation.")
+    elif userMac == "04:27:28":
+        print(f"For {userMac} the MAC manufacturer is Microsoft Corportation.")
+    elif userMac == "04:26:65":
+        print(f"For {userMac} the MAC manufacturer is Apple Inc.")
+    elif userMac == "04:33:89":
+        print(f"For {userMac} the MAC manufacturer is Huawei Technologies Co. Ltd.")
+    else:
+        print("This MAC manufacturer is unknown.")
 
-    # Define known MAC prefixes and corresponding manufacturers
-    mac_prefixes = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
-    manufacturers = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
-
-    # Default manufacturer value
-    manufacturer = "Unknown"
-
-    # Check for matching MAC prefix
-    for i in range(len(mac_prefixes)):
-        if mac_input.upper() == mac_prefixes[i].upper():
-            manufacturer = manufacturers[i]
-            break
-
-    # Print result
-    print("For {} the MAC manufacturer is {}.".format(mac_input, manufacturer))
-
-
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     main()
